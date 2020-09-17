084cca1f9ea022e8d0b886d28fdd8947d9642d3c
AlexIchenskiy <aichenskiy@gmail.com>
2020-09-17 19:01:49 +0200

    Updates

---
+++ precache-manifest.a6bbfc7e0427f0f744b908a33c1fdebe.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c119f3f72b7a130c56d278725aef0c2f",
+    "url": "/allien/index.html"
+  },
+  {
+    "revision": "ab2a0d3dcccf1b7fbd07",
+    "url": "/allien/static/css/main.737d72a6.chunk.css"
+  },
+  {
+    "revision": "0945ef71a980d3506707",
+    "url": "/allien/static/js/2.71fb7e41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/allien/static/js/2.71fb7e41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab2a0d3dcccf1b7fbd07",
+    "url": "/allien/static/js/main.e5bedf4b.chunk.js"
+  },
+  {
+    "revision": "1820a90b050971bba2b5",
+    "url": "/allien/static/js/runtime-main.d7567e9d.js"
+  },
+  {
+    "revision": "575981ee1ac41f1cdd270a193952ae64",
+    "url": "/allien/static/media/abg.575981ee.png"
+  },
+  {
+    "revision": "0ec2550c03121b9abcc9bdc57242c6c3",
+    "url": "/allien/static/media/allienbg.0ec2550c.png"
+  },
+  {
+    "revision": "7714aa8432e5231a469290c117f20fde",
+    "url": "/allien/static/media/collection.7714aa84.png"
+  },
+  {
+    "revision": "829d9bd900f6cde4a8f94e8c3d9028f7",
+    "url": "/allien/static/media/delivery.829d9bd9.svg"
+  },
+  {
+    "revision": "aef0dabc8a9c73ee6ea43cfd4827f812",
+    "url": "/allien/static/media/logo.aef0dabc.svg"
+  },
+  {
+    "revision": "e0d654d3d83dc6533d592756dedb8436",
+    "url": "/allien/static/media/lookbook.e0d654d3.png"
+  },
+  {
+    "revision": "16c2c3809671e8a1de7eaa734bfb521a",
+    "url": "/allien/static/media/returning.16c2c380.svg"
+  },
+  {
+    "revision": "27b7373be21a58db90aeaad02ff19041",
+    "url": "/allien/static/media/sales.27b7373b.svg"
+  },
+  {
+    "revision": "27ba320a91e905d2204cf06aa6168446",
+    "url": "/allien/static/media/uniform.27ba320a.svg"
+  },
+  {
+    "revision": "87bccb01b16b3cafaa2c32224ff80ad0",
+    "url": "/allien/static/media/whitelogo.87bccb01.svg"
+  }
+]);
