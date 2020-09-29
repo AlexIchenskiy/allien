0f5b4eb1abb9e35e030157ecd46c5fd96c3cc86a
AlexIchenskiy <aichenskiy@gmail.com>
2020-09-29 19:30:44 +0200

    1.4 Added radio check for carousel, small fixes

--- src/salecarousel.js
+++ src/salecarousel.js
@@ -14,16 +14,31 @@ class SaleCarousel extends React.Component {
 			names: [['Песочные шорты бермуды', '386грн.', '450грн.'], 
 					['Укороченный однотонный топ', '499грн.', '550грн.'], 
 					['Однотонный прямой пиджак', '999грн.', '1200грн.']],
-			current: 0,
+			current: 1,
 			isNext: true,
 			complete: false,
 		});
 
 		this.handlePrev = this.handlePrev.bind(this);
 		this.handleNext = this.handleNext.bind(this);
+		this.handleChange = this.handleChange.bind(this);
 	}
 
-	handleNext() {
+	handleChange(event) {
+		if (event.target.value == this.state.current) {
+			event.preventDefault();
+		} else if (!this.state.complete) {
+			this.setState({complete: true});
+			if (event.target.value > this.state.current) {
+				this.setState({current: parseInt(event.target.value), isNext: true});
+			} else if (event.target.value < this.state.current) {
+				this.setState({current: parseInt(event.target.value), isNext: false});
+			}
+			setTimeout(() => {this.setState({complete: false})}, 400);
+		}
+	}
+
+	handlePrev() {
 		if (!this.state.complete) {
 			this.setState({complete: true});
 			if (this.state.current-1 < 0) {
@@ -35,7 +50,7 @@ class SaleCarousel extends React.Component {
 		}
 	}
 
-	handlePrev() {
+	handleNext() {
 		if (!this.state.complete) {
 			this.setState({complete: true});
 			if (this.state.current+1 > this.state.pics.length-1) {
@@ -51,72 +66,91 @@ class SaleCarousel extends React.Component {
 		let next = this.state.current + 1 > this.state.pics.length-1 ? 0 : this.state.current + 1;
 		let prev = this.state.current - 1 < 0 ? this.state.pics.length-1 : this.state.current - 1;
 		return (
-			<div className = "sale-carousel">
-				<div className = "sale-carousel-left" onClick = {this.handlePrev}></div>
-				<ReactCSSTransitionGroup
-					transitionName = {{
-						enter: this.state.isNext ? 'enter-left-sale' : 'enter-right-sale',
-						enterActive: 'enter-active-sale',
-            			leave: 'leave-vert-sale',
-            			leaveActive: this.state.isNext ? 'leave-active-left-sale' : 'leave-active-right-sale'
-					}}
-				>
-		    	<div className = "sale-catalog-item" key = {prev+'sale'}>
-		        	<img src = {this.state.pics[prev]}/>
-		        	<div className = "sale-catalog-item-desc">
-		        		<span>{this.state.names[prev][0]}</span>
-		            	<div className = "bottom-catalog-color" style = {{backgroundColor: '#ffffff'}}>
-		            	</div>
-		            	<div className = "sale-catalog-price">
-		            		<span>{this.state.names[prev][1]+' грн.'}</span>
-		            		<div className = "price-arrow"></div>
-		          		</div>
-		        	</div>
-		        </div>
-		        </ReactCSSTransitionGroup>
-		        <ReactCSSTransitionGroup
-					transitionName = {{
-						enter: this.state.isNext ? 'enter-left-sale' : 'enter-right-sale',
-						enterActive: 'enter-active-sale',
-            			leave: 'leave-vert-sale',
-            			leaveActive: this.state.isNext ? 'leave-active-left-sale' : 'leave-active-right-sale'
-					}}
-				>
-		    	<div className = "sale-catalog-item sale-current" key = {this.state.current+'sale'}>
-		        	<img src = {this.state.pics[this.state.current]}/>
-		        	<div className = "sale-catalog-item-desc">
-		        		<span>{this.state.names[this.state.current][0]}</span>
-		            	<div className = "bottom-catalog-color" style = {{backgroundColor: '#ffffff'}}>
-		            	</div>
-		            	<div className = "sale-catalog-price">
-		            		<span>{this.state.names[this.state.current][1]+' грн.'}</span>
-		            		<div className = "price-arrow"></div>
-		          		</div>
-		        	</div>
-		        </div>
-		        </ReactCSSTransitionGroup>
-		        <ReactCSSTransitionGroup
-					transitionName = {{
-						enter: this.state.isNext ? 'enter-left-sale' : 'enter-right-sale',
-						enterActive: 'enter-active-sale',
-            			leave: 'leave-vert-sale',
-            			leaveActive: this.state.isNext ? 'leave-active-left-sale' : 'leave-active-right-sale'
-					}}
-				>
-		    	<div className = "sale-catalog-item" key = {next+'sale'}>
-		        	<img src = {this.state.pics[next]}/>
-		        	<div className = "sale-catalog-item-desc">
-		        		<span>{this.state.names[next][0]}</span>
-		            	<div className = "bottom-catalog-color" style = {{backgroundColor: '#ffffff'}}>
-		            	</div>
-		            	<div className = "sale-catalog-price">
-		            		<span>{this.state.names[next][1]+' грн.'}</span>
-		            		<div className = "price-arrow"></div>
-		          		</div>
-		        	</div>
-		        </div>
-		        </ReactCSSTransitionGroup>
-				<div className = "sale-carousel-right" onClick = {this.handleNext}></div>
+			<div className = "sale-carousel-outer">
+				<div className = "sale-carousel">
+					<div className = "sale-carousel-left" onClick = {this.handlePrev}></div>
+					<ReactCSSTransitionGroup
+						transitionName = {{
+							enter: this.state.isNext ? 'enter-left-sale' : 'enter-right-sale',
+							enterActive: 'enter-active-sale',
+	            			leave: 'leave-vert-sale',
+	            			leaveActive: this.state.isNext ? 'leave-active-left-sale' : 'leave-active-right-sale'
+						}}
+					>
+			    	<div className = "sale-catalog-item" key = {prev+'sale'}>
+			        	<img src = {this.state.pics[prev]}/>
+			        	<div className = "sale-catalog-item-desc">
+			        		<span>{this.state.names[prev][0]}</span>
+			            	<div className = "bottom-catalog-color" style = {{backgroundColor: '#ffffff'}}>
+			            	</div>
+			            	<div className = "sale-catalog-price">
+			            		<span>{this.state.names[prev][1]+' грн.'}</span>
+			            		<div className = "price-arrow"></div>
+			          		</div>
+			        	</div>
+			        </div>
+			        </ReactCSSTransitionGroup>
+			        <ReactCSSTransitionGroup
+						transitionName = {{
+							enter: this.state.isNext ? 'enter-left-sale' : 'enter-right-sale',
+							enterActive: 'enter-active-sale',
+	            			leave: 'leave-vert-sale',
+	            			leaveActive: this.state.isNext ? 'leave-active-left-sale' : 'leave-active-right-sale'
+						}}
+					>
+			    	<div className = "sale-catalog-item sale-current" key = {this.state.current+'sale'}>
+			        	<img src = {this.state.pics[this.state.current]}/>
+			        	<div className = "sale-catalog-item-desc">
+			        		<span>{this.state.names[this.state.current][0]}</span>
+			            	<div className = "bottom-catalog-color" style = {{backgroundColor: '#ffffff'}}>
+			            	</div>
+			            	<div className = "sale-catalog-price">
+			            		<span>{this.state.names[this.state.current][1]+' грн.'}</span>
+			            		<div className = "price-arrow"></div>
+			          		</div>
+			        	</div>
+			        </div>
+			        </ReactCSSTransitionGroup>
+			        <ReactCSSTransitionGroup
+						transitionName = {{
+							enter: this.state.isNext ? 'enter-left-sale' : 'enter-right-sale',
+							enterActive: 'enter-active-sale',
+	            			leave: 'leave-vert-sale',
+	            			leaveActive: this.state.isNext ? 'leave-active-left-sale' : 'leave-active-right-sale'
+						}}
+					>
+			    	<div className = "sale-catalog-item" key = {next+'sale'}>
+			        	<img src = {this.state.pics[next]}/>
+			        	<div className = "sale-catalog-item-desc">
+			        		<span>{this.state.names[next][0]}</span>
+			            	<div className = "bottom-catalog-color" style = {{backgroundColor: '#ffffff'}}>
+			            	</div>
+			            	<div className = "sale-catalog-price">
+			            		<span>{this.state.names[next][1]+' грн.'}</span>
+			            		<div className = "price-arrow"></div>
+			          		</div>
+			        	</div>
+			        </div>
+			        </ReactCSSTransitionGroup>
+					<div className = "sale-carousel-right" onClick = {this.handleNext}></div>
+				</div>
+				<div className = "sale-carousel-radio" onChange = {this.handleChange}>
+					<label className = "sale-radio-check">
+						<input type="radio" value="0" name="sale-check" 
+							checked = {this.state.current == 0 ? true : false}/>
+						<span className = "sale-carousel-check"></span>
+					</label>
+					<label className = "sale-radio-check">
+						<input type="radio" value="1" name="sale-check"
+							checked = {this.state.current == 1 ? true : false}/>
+						<span className = "sale-carousel-check"></span>
+					</label>
+					<label className = "sale-radio-check">
+						<input type="radio" value="2" name="sale-check"
+							checked = {this.state.current == 2 ? true : false}/>
+						<span className = "sale-carousel-check"></span>
+					</label>
+				</div>
 			</div>
 		)
 	}
